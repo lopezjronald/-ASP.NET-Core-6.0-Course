a119faef8474e10e7297957cfd2939b9bc2f9171
Ronald Lopez <lopez.j.ronald@gmail.com>
2022-11-14 19:33:49 -0800

    initial commit

---
+++ FirstMVCAppV2/Controllers/Exercise.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCAppV2.Controllers
+{
+    public class Exercise : Controller
+    {
+        public IActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstMVCAppV2/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCAppV2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Who is there";
+        }
+    }
+}
